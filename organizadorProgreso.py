4c3830a5b06c195fcf189008addd7fdad8080278
Miguell Criollo <macriollo.2@sudamericano.edu.ec>
2023-03-13 20:29:19 -0500

    Test tkinter

--- organizadorProgreso.py
+++ organizadorProgreso.py
@@ -1,4 +1,32 @@
+def main():
+    print("Hello, World!")
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+    
+'''
 from tkinter import *
 
-root = Tk()
-root.title("Mi programa")
+# Crear la ventana principal
+ventana = Tk()
+ventana.title("Hola")
+
+# Crear un widget Label para mostrar el mensaje "Hola"
+mensaje = Label(ventana, text="Hola")
+mensaje.pack()
+
+# Crear un widget Button para cerrar la ventana
+def cerrar_ventana():
+    ventana.destroy()
+
+boton_cerrar = Button(ventana, text="Cerrar", command=cerrar_ventana)
+boton_cerrar.pack()
+
+# Iniciar el loop principal de la ventana
+ventana.mainloop()
+'''
